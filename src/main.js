dbaf8f191235c85b9c59dabd5ff0d491e961b214
Tomeu Mayol <tomeu.mayol@etechyachting.com>
2025-03-31 20:29:41 +0200

    initialize project structure with Vue, Vite, and essential configurations

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({
+  // Configura las opciones que se adapten a tus necesidades
+  duration: 600,
+  once: true,
+});
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
